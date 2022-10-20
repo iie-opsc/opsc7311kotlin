241922974f8f9b5dad65d759cc22212ce6aa0394
Hermien Pellissier <miena.moo@gmail.com>
2022-10-20 23:44:43 +0200

    Implementing View.OnClickListener to avoid duplication of code

--- LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/MainActivity.kt
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/MainActivity.kt
@@ -2,39 +2,39 @@ package za.ac.iie.opsc7311.starsucks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import za.ac.iie.opsc7311.starsucks.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imgSb1.setOnClickListener() {
-            Toast.makeText(this@MainActivity, "MMM Soy Latte",
-                            Toast.LENGTH_SHORT).show()
-        }
-        binding.imgSb2.setOnClickListener() {
-            Toast.makeText(this@MainActivity, "MMM Chocco Frapp",
-                Toast.LENGTH_SHORT).show()
-        }
-        binding.imgSb3.setOnClickListener() {
-            Toast.makeText(this@MainActivity, "MMM Bottled Americano",
-                Toast.LENGTH_SHORT).show()
-        }
-        binding.imgSb4.setOnClickListener() {
-            Toast.makeText(this@MainActivity, "MMM Rainbow Frapp",
-                Toast.LENGTH_SHORT).show()
-        }
-        binding.imgSb5.setOnClickListener() {
-            Toast.makeText(this@MainActivity, "MMM Caramel Frapp",
-                Toast.LENGTH_SHORT).show()
-        }
-        binding.imgSb6.setOnClickListener() {
-            Toast.makeText(this@MainActivity, "MMM Black Forest Frapp",
-                Toast.LENGTH_SHORT).show()
+        binding.imgSb1.setOnClickListener(this)
+        binding.imgSb2.setOnClickListener(this)
+        binding.imgSb3.setOnClickListener(this)
+        binding.imgSb4.setOnClickListener(this)
+        binding.imgSb5.setOnClickListener(this)
+        binding.imgSb6.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.img_sb1 -> Toast.makeText(this@MainActivity,
+                "MMM Soy Latte", Toast.LENGTH_SHORT).show()
+            R.id.img_sb2 -> Toast.makeText(this@MainActivity,
+                "MMM Chocco Frapp", Toast.LENGTH_SHORT).show()
+            R.id.img_sb3 -> Toast.makeText(this@MainActivity,
+                "MMM Bottled Americano", Toast.LENGTH_SHORT).show()
+            R.id.img_sb4 -> Toast.makeText(this@MainActivity,
+                "MMM Rainbow Frapp", Toast.LENGTH_SHORT).show()
+            R.id.img_sb5 -> Toast.makeText(this@MainActivity,
+                "MMM Caramel Frapp", Toast.LENGTH_SHORT).show()
+            R.id.img_sb6 -> Toast.makeText(this@MainActivity,
+                "MMM Black Forest Frapp", Toast.LENGTH_SHORT).show()
         }
     }
 }
