bc2904dd368cd42bb886aa3261b485992b3af1ab
Hermien Pellissier <miena.moo@gmail.com>
2022-10-20 21:00:45 +0200

    Created initial copy from Learning Unit 2

---
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/MainActivity.kt
@@ -0,0 +1,11 @@
+package za.ac.iie.opsc7311.starsucks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
