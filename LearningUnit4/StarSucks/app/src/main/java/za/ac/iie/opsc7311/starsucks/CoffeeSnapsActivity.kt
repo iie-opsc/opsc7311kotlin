2d35e4ca8428b2b829ed01afdd78898db4169828
Hermien Pellissier <miena.moo@gmail.com>
2022-10-23 19:44:00 +0200

    Taking a photo with an intent

--- LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/CoffeeSnapsActivity.kt
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/CoffeeSnapsActivity.kt
@@ -1,11 +1,13 @@
 package za.ac.iie.opsc7311.starsucks
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
 import za.ac.iie.opsc7311.starsucks.databinding.ActivityCoffeeSnapsBinding
-import za.ac.iie.opsc7311.starsucks.databinding.ActivityMainWithNavDrawerBinding
 
 class CoffeeSnapsActivity : AppCompatActivity() {
 
@@ -15,5 +17,22 @@ class CoffeeSnapsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCoffeeSnapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // activity result launcher that will wait for the photo result
+        val getResult = registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult()
+            ) {
+                if(it.resultCode == Activity.RESULT_OK && it.data != null) {
+                    var bitmap = it.data!!.extras?.get("data") as Bitmap
+                    binding.imgCameraImage.setImageBitmap(bitmap)
+                }
+            }
+
+        binding.photoFab.setOnClickListener() {
+            // create the intent
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            // launch the intent
+            getResult.launch(intent)
+        }
     }
 }
