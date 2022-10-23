203d74e62a7c2614dcbdd9e8cb551feda3c47e9d
Hermien Pellissier <miena.moo@gmail.com>
2022-10-23 16:54:27 +0200

    Displaying pictures and added sharing

--- LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/OrderDetailsActivity.kt
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/OrderDetailsActivity.kt
@@ -19,7 +19,16 @@ class OrderDetailsActivity : AppCompatActivity() {
         binding.tvPlacedOrder.text = order.productName
 
         when(order.productName) {
+            "Soy Latte" -> binding.imgOrderedBeverage.setImageResource(R.drawable.sb1)
+            "Chocco Frapp" -> binding.imgOrderedBeverage.setImageResource(R.drawable.sb2)
+            "Bottled Americano" -> binding.imgOrderedBeverage.setImageResource(R.drawable.sb3)
+            "Rainbow Frapp" -> binding.imgOrderedBeverage.setImageResource(R.drawable.sb4)
+            "Caramel Frapp" -> binding.imgOrderedBeverage.setImageResource(R.drawable.sb5)
+            "Black Forest Frapp" -> binding.imgOrderedBeverage.setImageResource(R.drawable.sb6)
+        }
 
+        binding.fabOrder.setOnClickListener() {
+            shareIntent(applicationContext, order.productName)
         }
     }
 }
