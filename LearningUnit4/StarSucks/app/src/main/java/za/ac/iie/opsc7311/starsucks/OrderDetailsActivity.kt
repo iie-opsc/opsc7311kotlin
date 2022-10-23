f81663e30658d826bac58fe25cdbcfe1817247c7
Hermien Pellissier <miena.moo@gmail.com>
2022-10-23 11:40:54 +0200

    Getting the product name from the intent

--- LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/OrderDetailsActivity.kt
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/OrderDetailsActivity.kt
@@ -2,10 +2,24 @@ package za.ac.iie.opsc7311.starsucks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import za.ac.iie.opsc7311.starsucks.databinding.ActivityOrderDetailsBinding
 
 class OrderDetailsActivity : AppCompatActivity() {
+    var order = Order()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order_details)
+        val binding = ActivityOrderDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // get the name of the ordered product from the intent
+        order.productName = intent.getStringExtra("order").toString()
+
+        // set the product name on the text view
+        binding.tvPlacedOrder.text = order.productName
+
+        when(order.productName) {
+
+        }
     }
 }
