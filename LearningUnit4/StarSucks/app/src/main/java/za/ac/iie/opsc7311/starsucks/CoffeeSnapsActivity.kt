55491955ba07be8d8f57685eceb5dc3480ce32c5
Hermien Pellissier <miena.moo@gmail.com>
2022-10-25 22:25:48 +0200

    Taking a photo

--- LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/CoffeeSnapsActivity.kt
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/CoffeeSnapsActivity.kt
@@ -10,22 +10,32 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
 import za.ac.iie.opsc7311.starsucks.databinding.ActivityCoffeeSnapsBinding
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CoffeeSnapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoffeeSnapsBinding
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var cameraSelector: CameraSelector
+    private var imageCapture: ImageCapture? = null
+    private lateinit var imgCaptureExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCoffeeSnapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Create the executor
+        imgCaptureExecutor = Executors.newSingleThreadExecutor()
 
         // Ask for camera permission in case it wasn't granted already
         val cameraProviderResult = registerForActivityResult(
@@ -41,6 +51,23 @@ class CoffeeSnapsActivity : AppCompatActivity() {
             }
         // start the camera
         cameraProviderResult.launch(android.Manifest.permission.CAMERA)
+
+        // Add the code to the button to take the photo
+        // Based on https://developer.android.com/training/camerax/take-photo and
+        // https://www.section.io/engineering-education/how-to-implement-camerax-api-in-android/
+        binding.photoFab.setOnClickListener() {
+            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(
+                File(externalMediaDirs[0],"Coffee_${System.currentTimeMillis()}")).build()
+            imageCapture?.takePicture(outputFileOptions, imgCaptureExecutor,
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(error: ImageCaptureException) {
+                    }
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        Log.d("CoffeeSnapsActivity",
+                            "Photo saved to ${outputFileResults.savedUri}")
+                    }
+                })
+        }
     }
 
     // With thanks to the tutorial at
@@ -53,9 +80,11 @@ class CoffeeSnapsActivity : AppCompatActivity() {
             val preview = Preview.Builder().build().also{
                 it.setSurfaceProvider(binding.imgCameraImage.surfaceProvider)
             }
+            imageCapture = ImageCapture.Builder().build()
             try {
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview,
+                    imageCapture)
             } catch (e: Exception) {
                 Log.d("CoffeeSnapsActivity", "Use case binding failed")
             }
