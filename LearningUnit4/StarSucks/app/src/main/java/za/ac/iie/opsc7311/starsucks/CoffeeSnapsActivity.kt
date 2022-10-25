7048e8c5de09f31bc0dd8e1c228da2198fd6e380
Hermien Pellissier <miena.moo@gmail.com>
2022-10-25 21:40:57 +0200

    Removing old code using an intent

--- LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/CoffeeSnapsActivity.kt
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc7311/starsucks/CoffeeSnapsActivity.kt
@@ -18,21 +18,6 @@ class CoffeeSnapsActivity : AppCompatActivity() {
         binding = ActivityCoffeeSnapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // activity result launcher that will wait for the photo result
-        val getResult = registerForActivityResult(
-                ActivityResultContracts.StartActivityForResult()
-            ) {
-                if(it.resultCode == Activity.RESULT_OK && it.data != null) {
-                    var bitmap = it.data!!.extras?.get("data") as Bitmap
-                    binding.imgCameraImage.setImageBitmap(bitmap)
-                }
-            }
 
-        binding.photoFab.setOnClickListener() {
-            // create the intent
-            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            // launch the intent
-            getResult.launch(intent)
-        }
     }
 }
