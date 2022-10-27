2b3d3f7a2febc0901aab2b7be0acd210e0910350
Hermien Pellissier <miena.moo@gmail.com>
2022-10-27 20:42:52 +0200

    Created a second copy that will be used for Firestore

---
+++ LearningUnit5/StarSucksFirestore/app/src/main/java/za/ac/iie/opsc7311/starsucks/Order.kt
@@ -0,0 +1,22 @@
+package za.ac.iie.opsc7311.starsucks
+
+// primary constructor - no parameters
+class Order() {
+    lateinit var productName: String
+    lateinit var customerName: String
+    lateinit var customerCell: String
+    lateinit var orderDate: String
+
+    // secondary constructor
+    constructor(pName: String) : this() {
+        productName = pName
+    }
+
+    // secondary constructor
+    constructor(pName: String, cName: String, cCell: String,
+                oDate: String) : this(pName) {
+        customerName = cName
+        customerCell = cCell
+        orderDate = oDate
+    }
+}
