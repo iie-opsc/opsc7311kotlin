08f656d9e6f57df63d1c6cd6cfb5e6b3fb39441b
Hermien Pellissier <miena.moo@gmail.com>
2022-10-30 16:11:32 +0200

    Added cuppicakes and game over screen

--- LearningUnit6/BirdieGame/app/src/main/java/za/ac/iie/opsc7311/birdiegame/BirdieGameView.kt
+++ LearningUnit6/BirdieGame/app/src/main/java/za/ac/iie/opsc7311/birdiegame/BirdieGameView.kt
@@ -21,6 +21,7 @@ class BirdieGameView(context: Context?) : View(context) {
     // paint
     private var scoreCounter = Paint()
     private var levelCounter = Paint()
+    private var gameOver = Paint()
 
     // bird position and speed
     private var birdX = 10f
@@ -37,8 +38,14 @@ class BirdieGameView(context: Context?) : View(context) {
     private var pugiY = 0f
     private var pugiSpeed = 15
 
+    // rainbowcuppicake
+    private var rainbowX = -1f
+    private var rainbowY = 0f
+    private var rainbowSpeed = 20
+
     // player state
     private var score = 0
+    private var lifecount = 3
 
     init {
         scoreCounter.color = Color.BLACK
@@ -51,6 +58,12 @@ class BirdieGameView(context: Context?) : View(context) {
         levelCounter.typeface = Typeface.DEFAULT_BOLD
         levelCounter.textAlign = Paint.Align.CENTER
         levelCounter.isAntiAlias = true
+
+        gameOver.color = Color.RED
+        gameOver.textSize = 160f
+        gameOver.typeface = Typeface.DEFAULT_BOLD
+        gameOver.textAlign = Paint.Align.CENTER
+        gameOver.isAntiAlias = true
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -64,6 +77,13 @@ class BirdieGameView(context: Context?) : View(context) {
 
         // take careful note of the order here
         canvas?.drawBitmap(greengrassbackground, 0f, 0f, null)
+
+        // if the game is over, don't display all the rest, just the GAME OVER text
+        if (lifecount == 0) {
+            canvas?.drawText("GAME OVER!", (canvasWidth / 2).toFloat(),
+                (canvasHeight / 3).toFloat(), gameOver)
+            return
+        }
 
         // calculate the minimum Y value for the bird - one size below the top of the screen
         var minBirdY = bird.height
@@ -102,14 +122,39 @@ class BirdieGameView(context: Context?) : View(context) {
             pugiX = -100f
         }
 
+        // draw the cuppicake
+        rainbowX -= rainbowSpeed
+        if (rainbowX < 0) {
+            rainbowX = (canvasWidth + 200).toFloat()
+            rainbowY = (Math.floor(Math.random() * (maxBirdY - minBirdY)) + minBirdY).toFloat()
+        }
+        canvas?.drawBitmap(cuppicake, rainbowX, rainbowY, null)
+
+        // check for collision with cuppicake
+        if (collisionCheck(rainbowX, rainbowY)) {
+            rainbowX = -100f
+            lifecount--
+            Toast.makeText(this@BirdieGameView.context,
+                    "Boom! Death!", Toast.LENGTH_SHORT).show()
+            if (lifecount == 0) {
+                Toast.makeText(this@BirdieGameView.context,
+                    "GAME OVER!", Toast.LENGTH_LONG).show()
+            }
+        }
+
         // draw the score and level
         canvas?.drawText("Score: ${score}", 20f, 60f, scoreCounter)
         canvas?.drawText("Level 1", canvasWidth.toFloat()/2, 60f, levelCounter)
 
         // display the icons for the lives
-        canvas?.drawBitmap(lifealive, canvasWidth.toFloat() - 300, 30f, null)
-        canvas?.drawBitmap(lifealive, canvasWidth.toFloat() - 200, 30f, null)
-        canvas?.drawBitmap(lifealive, canvasWidth.toFloat() - 100, 30f, null)
+        for (position in 1..3) {
+            var xPosition = canvasWidth.toFloat() - (400 - position * 100)
+            if (lifecount < position) {
+                canvas?.drawBitmap(lifedead, xPosition, 30f, null)
+            } else {
+                canvas?.drawBitmap(lifealive, xPosition, 30f, null)
+            }
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
