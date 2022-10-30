a98c8cfa94d13d70d32b8ee03c630d2636b629ec
Hermien Pellissier <miena.moo@gmail.com>
2022-10-30 11:01:02 +0200

    Added properties and calculated bird limits

--- LearningUnit6/BirdieGame/app/src/main/java/za/ac/iie/opsc7311/birdiegame/BirdieGameView.kt
+++ LearningUnit6/BirdieGame/app/src/main/java/za/ac/iie/opsc7311/birdiegame/BirdieGameView.kt
@@ -6,6 +6,7 @@ import android.view.View
 
 class BirdieGameView(context: Context?) : View(context) {
 
+    // bitmaps
     private var bird : Bitmap = BitmapFactory.decodeResource(resources, R.drawable.wingsup)
     private var birdDown : Bitmap = BitmapFactory.decodeResource(resources, R.drawable.wingsdown)
     private var greengrassbackground : Bitmap = BitmapFactory.decodeResource(resources,
@@ -15,8 +16,19 @@ class BirdieGameView(context: Context?) : View(context) {
     private var lifealive : Bitmap = BitmapFactory.decodeResource(resources, R.drawable.smallalive)
     private var lifedead : Bitmap = BitmapFactory.decodeResource(resources, R.drawable.smalldead)
 
+    // paint
     private var scoreCounter = Paint()
     private var levelCounter = Paint()
+
+    // bird position and speed
+    private var birdX = 10f
+    private var birdY = 50f
+    private var birdSpeed = 10
+    private var touchFlag = false
+
+    // screen size
+    private var canvasHeight = 0
+    private var canvasWidth = 0
 
     init {
         scoreCounter.color = Color.BLACK
@@ -34,17 +46,34 @@ class BirdieGameView(context: Context?) : View(context) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
+        // get screen size
+        if (canvas != null) {
+            canvasHeight = canvas.height
+            canvasWidth = canvas.width
+        }
+
         // take careful note of the order here
         canvas?.drawBitmap(greengrassbackground, 0f, 0f, null)
+
+        // calculate the minimum Y value for the bird - one size below the top of the screen
+        var minBirdY = bird.height
+
+        // calculate the maximum Y value for the bird - one size above the bottom of the screen
+        var maxBirdY = canvasHeight - bird.height
+
+        // calculate the position of the bird
+        birdY += birdSpeed
+
+        // draw the bird
         canvas?.drawBitmap(bird, 0.0f, 0.0f, null)
 
         // draw the score and level
         canvas?.drawText("Score: 0", 20f, 60f, scoreCounter)
-        canvas?.drawText("Level 1", canvas.width.toFloat()/2, 60f, levelCounter)
+        canvas?.drawText("Level 1", canvasWidth.toFloat()/2, 60f, levelCounter)
 
         // display the icons for the lives
-        canvas?.drawBitmap(lifealive, canvas.width.toFloat() - 300, 30f, null)
-        canvas?.drawBitmap(lifealive, canvas.width.toFloat() - 200, 30f, null)
-        canvas?.drawBitmap(lifealive, canvas.width.toFloat() - 100, 30f, null)
+        canvas?.drawBitmap(lifealive, canvasWidth.toFloat() - 300, 30f, null)
+        canvas?.drawBitmap(lifealive, canvasWidth.toFloat() - 200, 30f, null)
+        canvas?.drawBitmap(lifealive, canvasWidth.toFloat() - 100, 30f, null)
     }
 }
